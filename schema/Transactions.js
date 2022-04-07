5583176572200897474b187eca185a84f3fe6f52
zolmine <68522174+zolmine@users.noreply.github.com>
2022-04-07 01:54:00 +0000

    submitting the newSchema

--- schema/Transactions.js
+++ schema/Transactions.js
@@ -1,1 +1,28 @@
-const schema = require("schema")
+const mongoose = require("mongoose");
+
+const pendingTransactios = new mongoose.Schema({
+    hash: {
+        type: String,
+        },
+    toAddress: {
+        type: String,
+    },
+    gas:{
+        type:String,
+    },
+    gasPrice:{
+        type:String,
+    },
+    maxFeePerGas:{
+        type:String,
+    },
+    maxPriorityFeePerGas:{
+        type:String,
+    },
+    time:{
+        type:String,
+    },
+},{timestamps:true});
+
+const Categorie = mongoose.model("pendingTransactios", pendingTransactios);
+module.exports = Categorie;
